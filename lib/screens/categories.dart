1aa550f7147022f20af468e7b4d12296492efdf9
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-11-05 15:37:48 -0800

    conditional category meals rendering

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/pseudo_data.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
-
+import 'package:meals/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+// responsible for handling page changes----------
+  void _selectedCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: ((context) => MealsScreen(title: category.title, meals: filteredMeals)),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,15 +28,20 @@ class CategoriesScreen extends StatelessWidget {
       body: GridView(
         padding: const EdgeInsets.all(20),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-          childAspectRatio: 3 / 2),
+            crossAxisCount: 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+            childAspectRatio: 3 / 2),
         children: [
           // for() is the same as running a map..
           // availableCategories.map((category) => CategoryGridItem(category: category).toList()
-          for(final category in availableCategories)
-            CategoryGridItem(category: category)
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectedCategory(context, category);
+              },
+            )
         ],
       ),
     );
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,7 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key});
+  const MealsScreen({required this.title, required this.meals, super.key});
+
+  final String title;
+  final List<Meal> meals;
+
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text('Uh oh.. nothing here!',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground)),
+          const SizedBox(height: 16),
+          Text(
+            'try selecting a different category!',
+            style: Theme.of(context)
+                .textTheme
+                .bodyLarge!
+                .copyWith(color: Theme.of(context).colorScheme.onBackground),
+          )
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+          itemCount: meals.length,
+          itemBuilder: ((context, index) => Text(meals[index].title)));
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: content,
+    );
+  }
 }
